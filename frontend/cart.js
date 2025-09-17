800656a97b9e051074b69b5d74b4760248b5aa88
Goutam Jena <goutamjena023@gmail.com>
2025-09-17 11:11:23 +0530

    fix the bug

---
+++ frontend/cart.js
@@ -0,0 +1,28 @@
+// frontend/cart.js
+class ShoppingCart {
+  constructor() {
+    this.items = [];
+  }
+
+  addItem(product) {
+    this.items.push(product);
+    console.log(`${product.name} added to cart.`);
+  }
+
+  removeItem(productId) {
+    this.items = this.items.filter(item => item.id !== productId);
+    console.log(`Item with ID ${productId} removed from cart.`);
+  }
+
+  // BUGFIX: Ensure the price is a number before adding it to the total
+  getTotal() {
+    return this.items.reduce((total, item) => total + (item.price || 0), 0);
+  }
+
+  getCartItems() {
+    return this.items;
+  }
+}
+
+const cart = new ShoppingCart();
+export default cart;
